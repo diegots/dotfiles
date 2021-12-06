f97f455c5d125b3a51ae3b8a8c6c22defb258126
Diego Trabazo Sardón <dtrabazo@gmail.com>
2021-12-06 19:38:21 +0100

    Add usage info

--- install-youtube-dl.sh
+++ install-youtube-dl.sh
@@ -8,3 +8,9 @@ dest_dir="$HOME/4.bin"
 curl -# -L https://yt-dl.org/downloads/latest/youtube-dl -o $dest_dir/youtube-dl
 chmod -v a+rx $dest_dir/youtube-dl
 
+# Usage
+# Start by listing all available contents from a resource
+# ./youtube-dl -F <url>
+
+# Then download the desired one:
+# ./youtube-dl -v -f <code_number> <url>
