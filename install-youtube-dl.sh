5bbe564dc6d2e1d32c17aa53cd31980d22507869
Diego Trabazo Sardón <dtrabazo@gmail.com>
2021-12-17 18:26:06 +0100

    Activate downloads for both yt dl apps

--- install-youtube-dl.sh
+++ install-youtube-dl.sh
@@ -5,12 +5,13 @@
 
 dest_dir="$HOME/4.bin"
 
-#url=https://yt-dl.org/downloads/latest/
-#app=youtube-dl
+url=https://yt-dl.org/downloads/latest/
+app=youtube-dl
+curl -# -L $url/$app -o $dest_dir/$app
+chmod -v a+rx $dest_dir/$app
 
 url=https://github.com/yt-dlp/yt-dlp/releases/latest/download/
 app=yt-dlp
-
 curl -# -L $url/$app -o $dest_dir/$app
 chmod -v a+rx $dest_dir/$app
 
