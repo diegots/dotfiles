9e7db7d798c9632dec883b24fb03516a966ed7f7
Diego Trabazo Sardón <dtrabazo@gmail.com>
2022-04-12 19:07:18 +0200

    Añade mc

--- install-packages.sh
+++ install-packages.sh
@@ -4,10 +4,11 @@
 sudo apt-get -y install mediainfo
 sudo apt-get -y install ffmpeg
 sudo apt-get -y install moc
-sudo apt-get -y install easytag
 sudo apt-get -y install sshfs
 sudo apt-get -y install tree
+sudo apt-get -y install mc
 
 # GUI
-sudo apt-get -y install meld
+#sudo apt-get -y install meld
+#sudo apt-get -y install easytag
 
